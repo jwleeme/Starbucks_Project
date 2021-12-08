291b2b0a30fdaa755632c315acbf75b8462544d2
izzy-92 <jiwonyy2@daum.net>
2021-12-08 14:25:30 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,22 +1,3 @@
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-// 서브메뉴 - 검색영역 input창 클릭이벤트 추가
-searchEl.addEventListener('click', function () {
-    // Logic...
-    searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', function () {
-    searchEl.classList.add('focused');
-    searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-searchInputEl.addEventListener('blur', function () {
-    searchEl.classList.remove('focused');
-    searchInputEl.setAttribute('placeholder', '');
-});
-
 // 사이드 배지 스크롤 이벤트
 const badgeEl = document.querySelector('header .sidebadges');
 const toTopEl = document.querySelector('#to-top'); //선택자를 여러번찾지않도록 아래 선택자 선언부분을 여기로옮겼다.
@@ -166,8 +147,3 @@ spyEls.forEach(function (secEl) {
     .setClassToggle(secEl,'show')
     .addTo(new ScrollMagic.Controller());
 });
-
-// 올해가 몇년도인지 자동계산 해주는 기능
-const thisYear = document.querySelector('.this-year');
-//textContent 속성 -> 그 요소가 가지고 있는 글자 내용들을 알아내거나 값을 지정하는 경우 사용.
-thisYear.textContent = new Date().getFullYear(); // 현재 년도 숫자데이터로 반환하는 내장함수사용
